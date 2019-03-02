1ee3cff2d72de305830c9ac9c2d67088f1db4877
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-03-02 13:08:33 +0100

    Changed API key

--- app/src/main/java/pl/com/tutorials/foodrecipes/util/AppValues.java
+++ app/src/main/java/pl/com/tutorials/foodrecipes/util/AppValues.java
@@ -3,8 +3,8 @@ package pl.com.tutorials.foodrecipes.util;
 public class AppValues {
 
     public static final String BASE_URL = "https://www.food2fork.com";
-//    public static final String API_KEY = "0e85f7984763157a6ee91b35ecfb64ba";
-    public static final String API_KEY = "436a6d9efb827514fa936bf4114ca9ff";
+    public static final String API_KEY = "0e85f7984763157a6ee91b35ecfb64ba";
+//    public static final String API_KEY = "436a6d9efb827514fa936bf4114ca9ff";
     public static final int NETWORK_TIMEOUT = 3000;
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES = {
